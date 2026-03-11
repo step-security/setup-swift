98760401df8f060a3c76a580f6391d68e5e429e1
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-03-11 13:47:11 +0530

    tests updated

--- src/const.ts
+++ src/const.ts
@@ -3,4 +3,4 @@ import * as path from 'path'
 export const MODULE_DIR = path.dirname(__dirname)
 export const SWIFTORG_ORIGIN = 'https://github.com/apple/swift-org-website.git'
 export const SWIFTORG_METADATA =
-  'https://swiftylab.github.io/setup-swift/metadata.json'
+  'https://step-security.github.io/setup-swift/metadata.json'
